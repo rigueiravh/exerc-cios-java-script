c13460c0a75e5b508958efa3eeb2513c0ef9b4c5
rigueira <rigueiravitorr@gmail.com>
2024-06-27 21:41:51 -0300

    feito da 1 ate a 9 (obs: correções a fazer).

---
+++ e.js
@@ -0,0 +1,84 @@
+let nome = 'joao'
+console.log(nome)
+
+// exercicio 02
+let a =5, b = 10, soma
+soma = a + b
+console.log(soma)
+
+//exercicio 03
+let primeironome = 'josefa', segundonome = 'silva'
+console.log(primeironome, segundonome)
+
+//exercicio 04
+let pessoa = {
+    nome : 'Cleiton',
+    idade : '18',
+    endereço : 'Ararangua',
+    telefone : 991028657,
+}
+
+//exercicio 05
+let numeros = [1,2,3,4,5]
+console.log(numeros[0], numeros[4])
+
+//exercicio 06
+let cores = ['vermelho', 'verde', 'azul']
+    console.log(cores)
+    cores.loop
+
+//exercicio 07
+let idade = 18
+if(idade < 18){
+    console.log('menor')
+}else{
+    console.log('maior')
+}
+
+//exrcicio 08
+let nota = 8
+if(nota > 7){
+    console.log('aprovada')
+
+}else if{
+    console.log('aprovada')
+}
+
+//exercico 09 
+let diadasemana = 7
+switch (diadasemana){
+    case 1: {
+        console.log('domingo')
+    }
+
+    break 
+    case 2: {
+        console.log('segunda')
+        break
+    }
+
+    case 3: {
+        console.log('terça')
+        break
+    }
+    
+    case 4: {
+        console.log('quarta')
+        break
+    }
+    
+    case 5: {
+        console.log('quinta')
+        break
+    }
+    
+    case 6: {
+        console.log('sexta')
+        break
+    }
+    
+    case 7: {
+        console.log('sabado')
+        break
+    }
+}
